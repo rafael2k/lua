b1c2cc5a1a2c2b03df23164167111b34c0d4a88b
Rafael Diniz <rafael@riseup.net>
2025-03-02 23:14:44 +0000

    mathlib random works!

--- src/lmathlib.c
+++ src/lmathlib.c
@@ -214,7 +214,8 @@ static int math_max (lua_State *L) {
 static int math_random (lua_State *L) {
   /* the `%' avoids the (rare) case of r==1, and is needed also because on
      some systems (SunOS!) `rand()' may return a value larger than RAND_MAX */
-  lua_Number r = (lua_Number)(rand()%RAND_MAX) / (lua_Number)RAND_MAX;
+  // lua_Number r = (lua_Number)(rand()%RAND_MAX) / (lua_Number)RAND_MAX;
+  lua_Number r = (lua_Number)(rand()%RAND_MAX);
   switch (lua_gettop(L)) {  /* check number of arguments */
     case 0: {  /* no arguments */
       lua_pushnumber(L, r);  /* Number between 0 and 1 */
@@ -224,7 +225,7 @@ static int math_random (lua_State *L) {
       int u = luaL_checkint(L, 1);
       luaL_argcheck(L, 1<=u, 1, "interval is empty");
 //      lua_pushnumber(L, floor(r*u)+1); // removed floor() by rafael2k - 01/03/2025
-      lua_pushnumber(L, (r*u)+1);  /* int between 1 and `u' */
+      lua_pushnumber(L, r % u);  /* int between 1 and `u' */
       break;
     }
     case 2: {  /* lower and upper limits */
@@ -232,7 +233,7 @@ static int math_random (lua_State *L) {
       int u = luaL_checkint(L, 2);
       luaL_argcheck(L, l<=u, 2, "interval is empty");
 //      lua_pushnumber(L, floor(r*(u-l+1))+l); // removed floor() by rafael2k - 01/03/2025
-      lua_pushnumber(L, (r*(u-l+1))+l);  /* int between `l' and `u' */
+      lua_pushnumber(L,  (r % (u - l)) + l);  /* int between `l' and `u' */
       break;
     }
     default: return luaL_error(L, "wrong number of arguments");
