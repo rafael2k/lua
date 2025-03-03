1cf7a35b53e7c6dbd0bb29c5f173460153d8c4ac
toncho11 <toncho11@gmail.com>
2025-03-03 15:31:58 +0100

    First implementation of VGA support.

--- src/lbaselib.c
+++ src/lbaselib.c
@@ -19,8 +19,8 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
-
-
+typedef unsigned char  byte;
+byte __far *VGA = (byte __far *)0xA0000000L;
 
 /*
 ** If your system does not support `stdout', you can just remove this function.
@@ -443,6 +443,44 @@ static int luaB_newproxy (lua_State *L) {
   return 1;
 }
 
+static int luaB_vga_init(lua_State *L)
+{
+lua_Number mode = luaL_checknumber(L, 1);  // Get the argument
+unsigned short int mode_int = (unsigned short int) mode;
+
+_asm{
+                push si
+                push di
+                push bp
+                push es
+                cli
+                mov ax,[mode_int]
+                int 0x10
+                sti
+                pop es
+                pop bp
+                pop di
+                pop si
+        }
+return 1;
+}
+
+static int luaB_plot_pixel(lua_State *L)
+{
+lua_Number number = luaL_checknumber(L, 1);
+unsigned int x = (unsigned int) number;
+
+number = luaL_checknumber(L, 2);
+unsigned int y = (unsigned int) number;
+
+number = luaL_checknumber(L, 3);
+unsigned int c = (unsigned int) number;
+
+/*  y*320 = y*256 + y*64 = y*2^8 + y*2^6   */
+VGA[(y<<8)+(y<<6)+x]=c;
+
+return 1;
+}
 
 static const luaL_Reg base_funcs[] = {
   {"assert", luaB_assert},
@@ -469,6 +507,8 @@ static const luaL_Reg base_funcs[] = {
   {"type", luaB_type},
   {"unpack", luaB_unpack},
   {"xpcall", luaB_xpcall},
+  {"vga_init", luaB_vga_init},
+  {"plot_pixel", luaB_plot_pixel},
   {NULL, NULL}
 };
 
@@ -650,4 +690,3 @@ LUALIB_API int luaopen_base (lua_State *L) {
   luaL_register(L, LUA_COLIBNAME, co_funcs);
   return 2;
 }
-
